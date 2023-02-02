5722cdd352adb6f183747371bd1746fa1e4adfb2
ayoub rezaei <ayoub.frontend1992@gmail.com>
2023-02-01 19:42:47 -0800

    done banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -12,16 +12,18 @@ import { motion } from 'framer-motion';
 import { fadeIn } from '../variants';
 
 const Banner = () => {
-  return <section  className='section' id='home'>
+  return <section  className='flex items-center min-h-[85vh] lg:min-h-[78vh]  ' id='home'>
 
     <div className="container mx-auto">
-      <div>
-          <div>
-            <h1>AYOUB <span>REZAEI</span></h1>
-            <div className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
+      <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
+          <div className='flex-1 text-center lg:text-left font-secondary'>
+            <motion.h1 variants={fadeIn('up',0.3)} initial="hidden" whileInView={'show'} viewport={{once:false,amount:0.7}} className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>AYOUB <span>REZAEI</span></motion.h1>
+            <motion.div 
+            variants={fadeIn('up',0.5)} initial="hidden" whileInView={'show'} viewport={{once:false,amount:0.7}}
+            className=' text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
               <span className='mr-4 text-white'>I am a</span>
               <TypeAnimation sequence={[
-                'Frontend developer',
+                'developer',
                 2000, 
                 'Photographer',
                 2000,
@@ -33,14 +35,38 @@ const Banner = () => {
               repeat={Infinity}
               className='text-accent'
               />
-              <p>
-                
-              </p>
-            </div>
+           
+            </motion.div>
+            <motion.p 
+            variants={fadeIn('up',0.6)} initial="hidden" whileInView={'show'} viewport={{once:false,amount:0.7}}
+            className=' max-w-lg mt-7 lg:mx-0 '>
+              I'm Ayoub Rezaei, i'm a Front-end developer with more than +3 years of experience developing.
+              </motion.p>
+              <motion.div
+              variants={fadeIn('up',0.6)} initial="hidden" whileInView={'show'} viewport={{once:false,amount:0.7}}
+              className='flex mt-7 max-w-max p-0 gap-x-6 items-center mx-auto lg:mx-0'>
+                <button className='btn btn-lg'  >Contact me</button>
+                <a href="#" className='text-gradient btn-link'>My Portfolio</a>
+              </motion.div>
+              <motion.div 
+              variants={fadeIn('up',0.7)} initial="hidden" whileInView={'show'} viewport={{once:false,amount:0.7}}
+              className=' flex tex-[20px] mt-10 gap-x-6 max-w-max mx-auto lg:mx-0 '>
+                <a href="#">
+                  <FaLinkedin/>
+                </a>
+                <a href="#">
+                  <FaGithub/>
+                </a>
+                <a href="#">
+                  <FaInstagram/>
+                </a>
+              </motion.div>
           </div>
-          <div>
+          <motion.div 
+          variants={fadeIn('down',0.5)} initial="hidden" whileInView={'show'}
+          className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]'>
             <img src={image} alt="banner-img" />
-          </div>
+          </motion.div>
       </div>
     </div>
 
